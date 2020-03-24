24acca2e38337be41dcbba21c39838907e8efaf0
David Palacios <davidpalacios@Davids-Mac-Pro.local>
2020-03-24 15:27:40 -0500

    added delete and add functionality

--- todoList/js/todos.js
+++ todoList/js/todos.js
@@ -1,6 +1,6 @@
 // cheeck off todos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
      // if li is gray
      // if ($(this).css("color") == "rgb(128, 128, 128)") {
      //      $(this).css("color", "black");
@@ -15,3 +15,23 @@ $("li").click(function(){
      // this one line toggles a css class
      $(this).toggleClass("completed");
 });
+
+// CLick on X to delete Todo
+$("ul").on("click", "span", function(event){
+     $(this).parent().fadeOut(500, function(){
+          $(this).remove();
+     });
+     event.stopPropagation();
+});
+
+// Enter a new todo
+$("input[type='text']").keypress(function(event){
+     if (event.which == 13) {
+          //grabbing the todo text from input text box
+          var todoText = $(this).val();
+          // create a new li and span and add to ul
+          $("ul").append("<li><span>X</span> " + todoText + "</li>");
+          // clear the input
+          $(this).val("");
+     }
+})
